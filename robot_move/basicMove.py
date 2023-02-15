78fb10686af7f3c2fce20a6e1bda14d6afc9de35
Atr-0 <MDLYQL@outlook.com>
2023-02-15 16:37:58 +0800

    line_sensor_subscription

--- robot_move/basicMove.py
+++ robot_move/basicMove.py
@@ -1,63 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import rclpy
-from rclpy.node import Node
+from rclpy.node import Node, Subscription
 import rclpy.qos
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Int32
 from rclpy.exceptions import ROSInterruptException
 import time
 import numpy as np
-'''
- [0,0,0,0]         [0,0,0,0,0,0]
- [0,0,0,0]         [0,0,0,0,0,0]
- [0,0,0,0]------>  [0,0,0,0,0,0]
- [0,0,0,0]         [0,0,0,0,0,0]
- [0,0,0,0]
- [0,0,0,0]
-'''
+np.set_printoptions(threshold=np.inf)
 sensor_matrix = np.zeros([6, 4])
 
 
-class basicMove(Node):
+class movement(Node):
     def __init__(self, vel, turnVel, dis):
         rclpy.init(args=None)
-        super().__init__("basicMove")
+        super().__init__("movement")
 
         self.pub = self.create_publisher(
             Twist, 'cmd_vel', 10)
         self.speed = vel
         self.turn = turnVel
-        self.x = 1.0
-        self.y = 0.0
+        self.x = 0.0
+        self.y = 1.0
         self.z = 0.0
         self.th = 1.0
-        self.create_rate(40)
+        self.rate = self.create_rate(500)
         self.twist = Twist()
+        node_sub = line_sensor_subscription()
 
-        self.line_sensor = self.create_subscription(
-            'line_sensor', Int32, self.line_sensor_callback())
-
-        while (dis >= 0):
-            time.sleep(0.02)
+        while dis > 0:
+            rclpy.spin_once(node_sub)
+            self.publish_twist()
+            print(sensor_matrix)
+            # self.rate.sleep()
             dis = dis-1
-
-    def line_sensor_callback(self, data):
-        temp = data.data
-        mijishu = 19
-        temp = 1024*1024-temp-1
-        global sv
-        for i in range(1, 7):
-            for j in range(1, 5):
-                sensor_matrix[i-1][4-j] = temp//2**mijishu
-                temp = temp % 2**mijishu
-                mijishu = mijishu-1
-            pass
-        pass
+        node_sub.destroy_node()
 
     def publish_twist(self):
         self.pub.publish(self.twist)
-
         self.twist.linear.x = self.x*self.speed
         self.twist.linear.y = self.y*self.speed
         self.twist.linear.z = self.z*self.speed
@@ -66,9 +47,32 @@ class basicMove(Node):
         self.twist.angular.z = self.th*self.turn
 
 
+class line_sensor_subscription(Node):
+    def __init__(self):
+        super().__init__("line_sensor_subscription")
+
+        self.create_subscription(
+            Int32, 'linesensor', self.line_sensor_callback, 10)
+        self.subscriptions
+
+    def line_sensor_callback(self, data):
+        temp = data.data
+        mijishu = 19
+        temp = 1024*1024-temp-1
+        global sensor_matrix
+        for i in range(1, 7):
+            for j in range(1, 5):
+                sensor_matrix[i-1][4-j] = temp//2**mijishu
+                temp = temp % 2**mijishu
+                mijishu = mijishu-1
+            pass
+        pass
+        # print(sensor_matrix)
+
+
 def main(args=None):
     rclpy.init(args=args)
-    node = basicMove()
+    node = movement()
     try:
         rclpy.spin(node)
     except ROSInterruptException:
@@ -84,9 +88,9 @@ def main(args=None):
         node.twist.angular.z = 0.0
 
         node.pub.publish(node.twist)
-        node.destroy_rate()
+
         node.destroy_node()
-        rclpy.shutdown(node)
+        rclpy.shutdown()
 
 
 if __name__ == "__main__":
--- robot_move/mainRuningProgm.py
+++ robot_move/mainRuningProgm.py
@@ -3,9 +3,19 @@ import basicMove
 import time
 
 
+def count_time(func):
+    def wrapper(*args, **kwargs):
+        t = time.time()
+        func(*args, **kwargs)
+        print('time ', time.time()-t)
+    return wrapper
+
+
+@count_time
 def main(args=None):
-    basicMove.getPose()
-    # basicMove.basicMove(-0.4,0,1)
+    # basicMove.getPose()
+    basicMove.movement(-0.4, 0, 4)
+    # time.sleep(10)
     # basicMove.basicMove(-0.4,0,3)
 
 
