0c5e4c46a162da1ab03c57883bac3f9b39c0f0f6
Atr-0 <MDLYQL@outlook.com>
2023-05-11 14:02:36 +0800

    i

--- robot_move/grab.py
+++ robot_move/grab.py
@@ -76,15 +76,15 @@ class grab():
 
     def a_zone_grab(self, pos=5):
         global a_zone_item_list
-        if a_zone_item_list == [0, 0, 1, 1, 2, 2,
-                                0, 0, 1, 1, 2, 2]:
+        if a_zone_item_list == [2, 2, 1, 1, 0, 0,
+                                2, 2, 1, 1, 0, 0]:
             if pos != 4:
                 basic.movement(6, -0.2*((4-pos)/abs(4-pos)),
                                0, 0.39*abs(4-pos), False, stop_weight=4)
             return
 
-        zone_num = 2 if (pos == 5 or pos == 4) else (
-            1 if (pos == 3 or pos == 2) else 0)
+        zone_num = 0 if (pos == 5 or pos == 4) else (
+            1 if (pos == 3 or pos == 2) else 2)
         grab_pos, push_pos, target_zone = -1, -1, -1
         range_value = range(5, -1, -1)
         if pos < 3:
@@ -311,8 +311,8 @@ class grab():
             "ADgai": 4,
         }
         '''
-        3 1
-        4 2
+        1 2
+        3 4
         '''
         grab_pos = -1
         goal_item = -1
@@ -331,7 +331,7 @@ class grab():
                             item_number = int(d_zone_item_list[i+(j*6)][num])
                             if (item_number // 10) % 10 == k and grab_pos == -1:
                                 grab_pos = i+(j*6)
-                                push_pos = 2 if (k == 2 or k == 4) else 3
+                                push_pos = 3 if (k == 2 or k == 4) else 2
                                 goal_item = k
                                 item_pos = item_number % 10
                                 item_index = num
